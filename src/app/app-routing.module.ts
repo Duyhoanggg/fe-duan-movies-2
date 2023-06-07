2d479cd36e3483faece2c97987e336f5919b3fef
Giangchuate <105421404+Giangchuate@users.noreply.github.com>
2023-06-07 21:57:26 +0700

    feat:fe-xay-dựng-layout-client refs:WA-35

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutClientComponent } from './layouts/base-layout-client/base-layout-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:BaseLayoutClientComponent,children:[]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
