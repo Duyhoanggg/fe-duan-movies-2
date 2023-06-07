a710bfb9fa34292fbe3a27a9b47cbd8b3b7c3bb1
Hoangldph21518 <Hoangldph21518@fpt.edu.vn>
2023-06-07 22:27:22 +0700

    feat: fe-xay-dựng-ui-movies-page refs: WA-37

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { BaseLayoutClientComponent } from './layouts/base-layout-client/base-layout-client.component';
 import { BaseLayoutAuthComponent } from './layouts/base-layout-auth/base-layout-auth.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MoviesPageComponent } from './pages/movies-page/movies-page.component';
 
 const routes: Routes = [
   {path:'',component:BaseLayoutClientComponent,children:[
-    {path:'',component:HomePageComponent}
+    {path:'',component:HomePageComponent},
+    {path:'movies',component:MoviesPageComponent}
   ]},
   {path:'',component:BaseLayoutAuthComponent,children:[]}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BaseLayoutAuthComponent } from './layouts/base-layout-auth/base-layout-
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BannerMovieComponent } from './components/banner-movie/banner-movie.component';
 import { ListMovieComponent } from './components/list-movie/list-movie.component';
+import { MoviesPageComponent } from './pages/movies-page/movies-page.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ListMovieComponent } from './components/list-movie/list-movie.component
     BaseLayoutAuthComponent,
     HomePageComponent,
     BannerMovieComponent,
-    ListMovieComponent
+    ListMovieComponent,
+    MoviesPageComponent
   ],
   imports: [
     BrowserModule,
